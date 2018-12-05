20c3fbbf481e0ad2c288dfba08aa770a3797f5ea
izaanjahangir <izaanjahangir2@gmail.com>
2018-12-05 18:31:16 +0500

    initialzed project and added configuration

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Learn React/Electron 2
           </a>
         </header>
       </div>
